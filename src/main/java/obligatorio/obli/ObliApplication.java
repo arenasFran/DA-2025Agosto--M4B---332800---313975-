064acd416c4d85916a0e22746b2690864c480f60
Fran <franarenas.code@gmail.com>
2025-10-08 21:32:00 -0300

    init

---
+++ src/main/java/obligatorio/obli/ObliApplication.java
@@ -0,0 +1,13 @@
+package obligatorio.obli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/obligatorio/obli/ObliApplicationTests.java
@@ -0,0 +1,13 @@
+package obligatorio.obli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
