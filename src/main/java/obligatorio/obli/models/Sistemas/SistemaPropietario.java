82cb87ef61444c18a755f1a2d74a37d8ad4414ec
Fran <franarenas.code@gmail.com>
2025-10-12 21:15:53 -0300

    orden

--- src/main/java/obligatorio/obli/models/Sistemas/SistemaPropietario.java
+++ src/main/java/obligatorio/obli/models/Sistemas/SistemaPropietario.java
@@ -1,5 +1,18 @@
-package obligatorio.obli.models;
+package obligatorio.obli.models.Sistemas;
+
+import java.util.List;
+
+import obligatorio.obli.models.Propietario;
 
 public class SistemaPropietario {
+    public List<Propietario> propietarios;
 
+    public Propietario getPropietarioPorCi(String Ci) {
+        for (Propietario p : propietarios) {
+            if (p.getCi() == Ci) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
