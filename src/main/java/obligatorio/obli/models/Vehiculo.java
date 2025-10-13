e56b981c3a1df21cdbc79dd9205474022d6bad69
andresmarpz <andresmarpz@gmail.com>
2025-10-12 21:03:40 -0300

    vehiculo con Categoria

--- src/main/java/obligatorio/obli/models/Vehiculo.java
+++ src/main/java/obligatorio/obli/models/Vehiculo.java
@@ -4,5 +4,28 @@ public class Vehiculo {
     private String matricula;
     private String modelo;
     private String color;
-    private String categoria;
+    private Categoria categoria;
+
+    public Vehiculo(String matricula, String modelo, String color, Categoria categoria) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.color = color;
+        this.categoria = categoria;
+    }
+    
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
 }
