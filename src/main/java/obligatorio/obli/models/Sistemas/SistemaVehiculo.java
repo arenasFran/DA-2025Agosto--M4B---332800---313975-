9e5d6823448749c3d764a57f3a43335e5738b5a9
Fran <franarenas.code@gmail.com>
2025-10-12 21:01:13 -0300

    Sistema vehiculo

--- src/main/java/obligatorio/obli/models/Sistemas/SistemaVehiculo.java
+++ src/main/java/obligatorio/obli/models/Sistemas/SistemaVehiculo.java
@@ -1,5 +1,21 @@
-package obligatorio.obli.models;
+package obligatorio.obli.models.Sistemas;
+
+import java.util.List;
+import obligatorio.obli.models.Vehiculo;
 
 public class SistemaVehiculo {
+    private List<Vehiculo> vehiculos;
 
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public Vehiculo getVehiculoPorMatricula(String matricula) {
+        for (Vehiculo v : vehiculos) {
+            if (v.getMatricula() == matricula) {
+                return v;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/obligatorio/obli/models/Vehiculo.java
+++ src/main/java/obligatorio/obli/models/Vehiculo.java
@@ -5,4 +5,36 @@ public class Vehiculo {
     private String modelo;
     private String color;
     private String categoria;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
