f65f54391b8610508b8282dbe44b67a42ef0a065
andresmarpz <andresmarpz@gmail.com>
2025-10-14 21:38:01 -0300

    crear modelo Administrador

---
+++ src/main/java/obligatorio/obli/models/Usuarios/Administrador.java
@@ -0,0 +1,7 @@
+package obligatorio.obli.models.Usuarios;
+
+public class Administrador extends User {
+  public Administrador(int id, String ci, String nombre, String password) {
+    super(id, ci, nombre, password);
+  }
+}
